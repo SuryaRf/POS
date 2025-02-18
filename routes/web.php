09e25abc80f06e2b3c8f9bedc9c1fb0281e3d082
Keysha Arindra Fabian <arindrakeysha@gmail.com>
2025-02-18 13:53:03 +0700

    MVC-Routing-Basic

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,90 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
+
+Route::get('/world', function () {
+    return 'World';
 });
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId ." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+// Route::get('/user/profile', function () {
+//     //
+// })->name('profile');
+
+// Route::get(
+//     '/user/profile',
+//     [UserProfileController::class, 'show']
+// )->name('profile');
+
+// // Generating URLs...
+// $url = route('profile');
+
+// // Generating Redirects...
+// return redirect()->route('profile');
+
+// Route::middleware(['first', 'second'])->group(function () {
+//     Route::get('/', function () {
+//         // Uses first & second middleware...
+//     });
+
+// Route::get('/user/profile', function () {
+//         // Uses first & second middleware...
+//     });
+// });
+
+// Route::domain('{account}.example.com')->group(function () {
+//     Route::get('user/{id}', function ($account, $id) {
+//         //
+//     });
+// });
+
+// Route::middleware('auth')->group(function () {
+// 	Route::get('/user', [UserController::class, 'index']);
+// 	Route::get('/post', [PostController::class, 'index']);
+// 	Route::get('/event', [EventController::class, 'index']);
+	
+// });
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
+// Route::get('/greeting', function () {
+// 	return view('blog.hello', ['name' => 'Keysha']);
+// });
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
