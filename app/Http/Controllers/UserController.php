986b4dcfaffca6348a2d8cd92734facdbadc76d1
Keysha Arindra Fabian <arindrakeysha@gmail.com>
2025-03-04 13:17:12 +0700

    prak-1-js-4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,15 +14,18 @@ class UserController extends Controller
     public function index()
     {
 
-        // tambah data user dengan Eloquent Model
+        //tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data);
 
         // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        $users = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $users]);
     }
 
     public function user()
