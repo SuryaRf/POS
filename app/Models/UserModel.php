8dd007476cde8d59cf74df0b21bab5aac4523ef3
Keysha Arindra Fabian <arindrakeysha@gmail.com>
2025-03-04 15:23:03 +0700

    prak-2.6dan2.7-js-4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use App\Models\LevelModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
@@ -16,4 +18,9 @@ class UserModel extends Model
     // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
