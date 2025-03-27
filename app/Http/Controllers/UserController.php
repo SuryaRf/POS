75cd2c2b7e26ee75fab53e70cdb5269b5a21a81f
Keysha Arindra Fabian <arindrakeysha@gmail.com>
2025-03-27 22:18:27 +0700

    filtering & searching js5 prak 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,15 +23,21 @@ class UserController extends Controller
         ];
         $activeMenu = 'user'; // Set menu yang sedang aktif
 
-        return view('user.index', compact('breadcrumb', 'page', 'activeMenu'));
+        $level = LevelModel::all();
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk JSON untuk DataTables
     public function list(Request $request)
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
-            ->with('level')
-            ->orderBy('user_id', 'asc'); // Urutkan berdasarkan user_id
+            ->with('level');
+            // ->orderBy('user_id', 'asc'); // Urutkan berdasarkan user_id
+        
+        if ($request -> level_id) {
+            $users->where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             ->addIndexColumn() // Menambahkan kolom index otomatis
