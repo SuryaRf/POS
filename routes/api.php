849e965613af0cc303516fc1b85bb4dc77099987
SuryaRf <ayruzrahmad@gmail.com>
2025-04-29 23:37:15 +0700

    Membuat RESTful API Logout

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,5 @@ Route::post('/register', RegisterController::class)->name('register');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
